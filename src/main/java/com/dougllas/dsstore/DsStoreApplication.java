6201c59bdb4d9564df557b49a037b2b893a2baa6
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-12 00:25:30 -0300

    build: Cria o projeto

---
+++ src/main/java/com/dougllas/dsstore/DsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.dougllas.dsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dougllas/dsstore/DsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dougllas.dsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
